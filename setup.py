1e545b5992946c1b94ff48c76edac20530f1c2b2
thomasjohn216 <thomasjohn.ibm@yahoo.com>
2017-06-05 22:49:32 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(name='pixiedust_twitterdemo',
+      version='0.7',
+      description='Pixiedust 2017 thomas john demo of the Twitter Sentiment Analysis tutorials',
+      url='https://github.com/thomasjohn216/twitterdemo',
+      install_requires=['pixiedust'],
+      author='Thomas John',
+      author_email='thomasjohn.ibm@yahoo.com',
+      license='Apache 2.0',
+      packages=['pixiedust_twitterdemo'],
+      include_package_data=True,
+      zip_safe=False)
